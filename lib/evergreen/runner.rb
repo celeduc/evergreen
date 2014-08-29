d747e7c635565a9c0a2adf3481196d8d9122de76
Chuck LeDuc Díaz <chuck.leduc@sage.com>
2014-08-29 13:07:55 +0200

    double timeout again (experiment)

--- lib/evergreen/runner.rb
+++ lib/evergreen/runner.rb
@@ -49,7 +49,7 @@ module Evergreen
 
           previous_results = ""
 
-          Evergreen.timeout(600) do
+          Evergreen.timeout(1200) do
             dots = session.evaluate_script('Evergreen.dots')
             io.print dots.sub(/^#{Regexp.escape(previous_results)}/, '')
             io.flush
